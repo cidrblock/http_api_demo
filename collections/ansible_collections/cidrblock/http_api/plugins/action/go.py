061ed997b5d4e043c8342ddd6fab537f203dba76
cidrblock <bthornto@redhat.com>
2022-03-28 15:24:36 -0700

    init

---
+++ collections/ansible_collections/cidrblock/http_api/plugins/action/go.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+
+from ansible.errors import AnsibleActionFail
+from ansible.plugins.action import ActionBase
+
+
+class ActionModule(ActionBase):
+    def run(self, tmp=None, task_vars=None):
+
+        module = super(ActionModule, self).run(tmp, task_vars)
+        connection = self._connection
+        connection._connect()
+        return {
+            "username": connection.get_option("remote_user"),
+            "password": connection.get_option("password"),
+        }
